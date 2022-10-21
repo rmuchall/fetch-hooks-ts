a855884c5d60ff3c04a0bb1d4b9e0cb88ae0d4b5
Rob Muchall <rmuchall@protonmail.com>
2022-10-21 20:33:10 +0100

    Revert barrel changes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
 // core
-import {FetchHooks} from "./FetchHooks.js";
-
+export * from "./FetchHooks.js";
 // interfaces
-import {InstanceOptions} from "./interfaces/InstanceOptions.js";
-import {RequestOptions} from "./interfaces/RequestOptions.js";
+export * from "./interfaces/InstanceOptions.js";
+export * from "./interfaces/RequestOptions.js";
 // utilities
-import {HeadersInitOrUndefined, BeforeRequestHook, AfterResponseHook} from "./utilities/type-aliases.js";
-
-export {
-    FetchHooks,
-    InstanceOptions,
-    RequestOptions,
-    HeadersInitOrUndefined,
-    BeforeRequestHook,
-    AfterResponseHook
-};
+export * from "./utilities/type-aliases.js";
